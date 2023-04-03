c315c55eb109b61317b5cba655255a5bf6d5da32
Luis Molinuevo <luismolinuevo0@gmail.com>
2023-04-03 18:01:54 -0400

    setup the server.js file

---
+++ server/server.js
@@ -0,0 +1,19 @@
+import express from "express";
+import morgan from "morgan";
+import cors from "cors";
+// import setupJWTStrategy from "./middlewares/auth.js";
+// import passport from "passport";
+
+export default function createServer() {
+    const app = express();
+    app.use(cors())
+    app.use(express.json());
+
+    app.use(morgan("tiny"));
+
+    // setupJWTStrategy(passport);
+
+    // app.use("/auth", authRouter);
+
+    return app;
+}
