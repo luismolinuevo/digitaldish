dddf2a24777d8e0605b838ed8daf43a299387c88
Luis Molinuevo <luismolinuevo0@gmail.com>
2023-04-04 18:44:58 -0400

    joinroom and sendmessage socket setup

--- server/server.js
+++ server/server.js
@@ -1,4 +1,6 @@
 import express from "express";
+import http from "http";
+import { Server } from "socket.io"
 import morgan from "morgan";
 import cors from "cors";
 import AuthRouter from "./routes/auth.js";
@@ -18,6 +20,32 @@ export default function createServer() {
     app.use("/post", postRouter)
     app.use("/chat",chatRouter);
     app.use("/auth", AuthRouter);
+
+
+    //set up socket server
+    const server = http.createServer(app);
+    const io = new Server(server);
+
+    io.on("connection", (socket) => {
+        socket.on("joinRoom", (roomId) => {
+            socket.join(roomId)
+        })
+
+        socket.on("sendMessage", async (message, roomId) => {
+            const newMessage = await prisma.message.create({
+                data: {
+                    content: message.content,
+                    senderUserName: message.senderUserName,
+                    createAt: new Date(),
+                    chat: {
+                        connect: {id: roomId},
+                    },
+                }
+            });
+
+            io.to(roomId).emit("newMessage");
+        });
+    })
 
     return app;
 }
