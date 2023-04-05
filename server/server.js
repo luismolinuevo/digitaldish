48db462f44d71d9a940b3bfea6e9559193923b44
Luis Molinuevo <luismolinuevo0@gmail.com>
2023-04-04 20:00:30 -0400

    get chatroom by chatroom id routed created

--- server/server.js
+++ server/server.js
@@ -4,6 +4,7 @@ import { Server } from "socket.io"
 import morgan from "morgan";
 import cors from "cors";
 import chatRouter from "./routes/chat.js";
+import prisma from "./db/index.js";
 // import setupJWTStrategy from "./middlewares/auth.js";
 // import passport from "passport";
 
@@ -25,7 +26,7 @@ export default function createServer() {
 
     io.on("connection", (socket) => {
         socket.on("joinRoom", (roomId) => {
-            socket.join(roomId)
+            socket.join(roomId);
         })
 
         socket.on("sendMessage", async (message, roomId) => {
