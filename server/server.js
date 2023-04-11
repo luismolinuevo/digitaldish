ea650309d04deab59049d6f1bdc9335bec2385cd
Luis Molinuevo <luismolinuevo0@gmail.com>
2023-04-11 18:29:29 -0400

    socket.io added

--- server/server.js
+++ server/server.js
@@ -1,9 +1,12 @@
 import express from "express";
+import http from "http";
+import { Server } from "socket.io"
 import morgan from "morgan";
 import cors from "cors";
 import authRouter from "./routes/auth.js";
 import chatRouter from "./routes/chat.js";
 import postRouter from "./routes/items.js"
+import prisma from "./db/index.js";
 import setupJWTStrategy from "./auth/index.js";
 import passport from "passport";
 
@@ -16,8 +19,35 @@ export default function createServer() {
 
     setupJWTStrategy(passport);
     app.use("/post", postRouter)
-    app.use("/chat", chatRouter);
+    app.use("/chat",chatRouter);
     app.use("/auth", authRouter);
+
+
+    //set up socket server
+    const server = http.createServer(app);
+    const io = new Server(server);
+
+    io.on("connection", (socket) => {
+        
+        socket.on("joinRoom", (roomId) => {
+            socket.join(roomId);
+        })
+
+        socket.on("sendMessage", async (message, roomId) => {
+            const newMessage = await prisma.message.create({
+                data: {
+                    content: message.content,
+                    senderUserName: message.senderUserName,
+                    createAt: new Date(),
+                    chat: {
+                        connect: {id: roomId},
+                    },
+                }
+            });
+
+            io.to(roomId).emit("newMessage");
+        });
+    })
 
     return app;
 }
