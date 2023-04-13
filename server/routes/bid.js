5179484510adca4bbbdc609b8b2f6579dbba88e4
Luis Molinuevo <luismolinuevo0@gmail.com>
2023-04-13 02:12:42 -0400

    post route for bid created

--- server/routes/bid.js
+++ server/routes/bid.js
@@ -3,6 +3,20 @@ import prisma from "../db/index.js";
 
 const router = express.Router();
 
+//create a bid
+router.post("/:postId", async (req, res) => {
+    const postId = req.params.postId;
 
+    const createBid = prisma.bid.create({
+        data: {
+            price: req.body.price,
+            postId: Number(postId)
+        }
+    });
+
+    res.status(201).json({
+        success: true,
+    });
+});
 
 export default router;
