0dd74be354526548fc79d0b7d6e87510c377f98f
Kwame Bullen <34757037+Adam43@users.noreply.github.com>
2023-04-22 22:49:28 -0400

    setup client side router

--- client/frontend/src/main.jsx
+++ client/frontend/src/main.jsx
@@ -1,10 +1,51 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
+import { createBrowserRouter, RouterProvider } from "react-router-dom"
+
 import './index.css'
+
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <div>Hello World!</div>,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/profile",
+        element: <Profile />
+      },
+      {
+        path: "/following",
+        element: <Following />
+      },
+      {
+        path: "/listing",
+        element: <Listing />,
+      },
+      {
+        path: "/signup",
+        element: <Signup />
+      },
+      {
+        path: "login",
+        element: <Login />
+      },
+      {
+        path: "logout",
+        element: <Logout />
+      }
+    ]
+  },
+]);
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
-)
+);
