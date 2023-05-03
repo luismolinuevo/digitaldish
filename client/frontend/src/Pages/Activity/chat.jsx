a476ef39f453f21c0e08fee4c10f1dd467924077
Luis Molinuevo <luismolinuevo0@gmail.com>
2023-05-02 20:57:47 -0400

    auto scolls when new message is entered

--- client/frontend/src/Pages/Activity/chat.jsx
+++ client/frontend/src/Pages/Activity/chat.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import axios from "axios";
 import { io } from "socket.io-client";
 import { useSelector, useDispatch } from "react-redux";
@@ -18,16 +18,17 @@ export default function Chat() {
   const dispatch = useDispatch();
   const offerId = useSelector((state) => state.offers.currentOffer);
   const user = useSelector((state) => state.auth.userInfo);
-  console.log(user)
+  console.log(user);
   // console.log(offerId)
 
   const [messages, setMessages] = useState([]);
   const [inputMessage, setInputMessage] = useState("");
+  const messagesEndRef = useRef(null);
 
   // const socket = io();
 
   useEffect(() => {
-    dispatch(checkLoginStatus())
+    dispatch(checkLoginStatus());
     // const socket = io.connect("http://localhost:8080/");
     // Join the offer room
     socket.emit("connection", "Hi there");
@@ -47,6 +48,10 @@ export default function Chat() {
       socket.off("newMessage");
     };
   }, [offerId]); //putting offerId here made it so that this runs whenever offerId has changed
+
+  useEffect(() => {
+    scrollToBottom();
+  }, [messages]);
 
   const fetchMessages = async () => {
     try {
@@ -79,6 +84,10 @@ export default function Chat() {
     }
   };
 
+  const scrollToBottom = () => {
+    messagesEndRef.current.scrollIntoView({ behavior: "smooth" });
+  };
+
   return (
     <div>
       <div className="w-[520px] h-[820px] border-black border-[2px] relative">
@@ -98,14 +107,22 @@ export default function Chat() {
         <div className="h-full">
           <div className="h-[60%] overflow-y-scroll">
             {messages.length != 0 ? (
-              messages.map((items) => 
-              <div className={items.userId === user ? "flex justify-end" : "flex  justify-start"}>
-              <p className={`flex w-[350px] h-[32px] rounded-[50px] bg-[#D9D9D9] mx-[25px] mb-[16px]`}>{items.content}</p>
-              </div>
-            )
+              messages.map((items) => (
+                <div
+                  className={`mx-[25px] mb-[16px] ${items.userId === user? "flex justify-end": "flex justify-start"}`}
+                  key={items.id}
+                >
+                  <p
+                    className={`w-[350px] h-[32px] rounded-[50px] bg-[#D9D9D9] text-[12px] p-1`}
+                  >
+                    {items.content}
+                  </p>
+                </div>
+              ))
             ) : (
               <p></p>
             )}
+            <div ref={messagesEndRef} />
           </div>
           <div className="absolute bottom-0 w-full">
             <div className="mb-4 mt-6 flex justify-center">
