40f07dd08809c1752f41e5ae3e691d4d581577c2
Luis Molinuevo <luismolinuevo0@gmail.com>
2023-05-03 23:13:39 -0400

    created specfic negotation listing page

---
+++ client/frontend/src/Pages/Listing/SpecificNegotiate.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+export default function SpecificNegotiate() {
+  
+  return (
+    <div>
+        <div className='flex flex-col'>
+            <div className='flex'>
+              <p>negotiation</p>
+              <p>&gt;</p>
+            </div>
+            <div className='flex'>
+                <div>
+
+                </div>
+                <div>
+
+                </div>
+            </div>
+        </div>
+    </div>
+  )
+}
