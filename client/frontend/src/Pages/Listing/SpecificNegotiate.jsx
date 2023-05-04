25885c4c463f57be625a6f0fd357397817c0bfbd
Luis Molinuevo <luismolinuevo0@gmail.com>
2023-05-03 23:18:10 -0400

    created specfic negotation listing test route

--- client/frontend/src/Pages/Listing/SpecificNegotiate.jsx
+++ client/frontend/src/Pages/Listing/SpecificNegotiate.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 
 export default function SpecificNegotiate() {
-  
+  //TODO make api call and use useParams to get specfic listing. Update post model to support type of transaction
   return (
     <div>
         <div className='flex flex-col'>
--- client/frontend/src/main.jsx
+++ client/frontend/src/main.jsx
@@ -14,6 +14,7 @@ import Layout from './Components/Layout'
 
 import { Provider } from 'react-redux'
 import store from "./Utils/store"
+import SpecificNegotiate from './Pages/Listing/SpecificNegotiate'
 
 
 
@@ -49,6 +50,18 @@ const router = createBrowserRouter([
       {
         path: "/logout",
         element: <Logout />
+      },
+
+
+
+
+
+
+
+
+      {
+        path: "/specnegotiatelisting",
+        element: <SpecificNegotiate/>
       }
     ]
   },
