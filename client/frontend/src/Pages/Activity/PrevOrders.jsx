cf3db8be93fa02c95be5033f47a8756507d0a7ba
Luis Molinuevo <89353175+luismolinuevo@users.noreply.github.com>
2023-05-16 15:15:03 -0400

    mock data replaced with real data except img and status

--- client/frontend/src/Pages/Activity/PrevOrders.jsx
+++ client/frontend/src/Pages/Activity/PrevOrders.jsx
@@ -19,7 +19,6 @@ export default function PrevOrders() {
       if (fetchData.status === 200) {
         setPrevOrder(fetchData.data.getPrevOrders);
         console.log(fetchData.data.getPrevOrders);
-
       }
     } catch (error) {
       console.log("Error fetching prev orders");
@@ -35,20 +34,23 @@ export default function PrevOrders() {
       </div>
       {prevOrder && prevOrder.length != 0 ? (
         prevOrder.map((item) => (
-          <div className="flex w-[1171px] h-[120px] border-[1px] border-black justify-between" key={item.id}>
+          <div
+            className="flex w-[1171px] h-[120px] border-[1px] border-black justify-between"
+            key={item.id}
+          >
             <div className="flex">
+              <img></img>
               <div className="py-3">
                 <h1 className="text-[27px]">{item.post.title}</h1>
                 <p className="text-[27px]">{item.post.userName}</p>
               </div>
-              <img></img>
+
               <div className=" flex items-center px-5 w-[375px] ">
                 <h1 className="text-[27px]">status</h1>
               </div>
             </div>
             <div className="flex items-center">
-              <h1 className="text-[27px]">Expired</h1>
-              <h1 className="text-[27px] pl-[65px] px-[10px]">Sold: $00.00</h1>
+              <h1 className="text-[27px]">{item.finalPrice}</h1>
             </div>
           </div>
         ))
