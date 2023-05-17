6fb8c578fbc6ebf6610cb44391690b0d2ccd2e97
Luis Molinuevo <89353175+luismolinuevo@users.noreply.github.com>
2023-05-16 21:34:26 -0400

    count down done and working

--- client/frontend/src/Components/Countdown.jsx
+++ client/frontend/src/Components/Countdown.jsx
@@ -1,44 +1,60 @@
-import React, {useState, useEffect} from 'react'
+import React, { useState, useEffect } from "react";
 
-export default function Countdown({startDate, endDate}) {
-    const [days, setDays] = useState(0);
-    const [hours, setHours] = useState(0);
-    const [minutes, setMinutes] = useState(0);
-    const [seconds, setSeconds] = useState(0);
-  
-    const calculateTimeLeft = () => {
-      const start = new Date(startDate).getTime();
-      const end = new Date(endDate).getTime();
-      const totalSeconds = (end - start) / 1000;
-  
-      if (totalSeconds <= 0) {
-        setDays(0);
-        setHours(0);
-        setMinutes(0);
-        setSeconds(0);
-        return;
-      }
-  
-      const daysLeft = Math.floor(totalSeconds / 86400);
-      const hoursLeft = Math.floor((totalSeconds % 86400) / 3600);
-      const minutesLeft = Math.floor((totalSeconds % 3600) / 60);
-      const secondsLeft = Math.floor(totalSeconds % 60);
-  
-      setDays(daysLeft);
-      setHours(hoursLeft);
-      setMinutes(minutesLeft);
-      setSeconds(secondsLeft);
-    };
+const LiveCountdown = ({ startDate, endDate }) => {
+  const [days, setDays] = useState(0);
+  const [hours, setHours] = useState(0);
+  const [minutes, setMinutes] = useState(0);
+  const [seconds, setSeconds] = useState(0);
 
-    useEffect(() => {
-        const timer = setInterval(() => calculateTimeLeft(), 1000);
-        return () => clearInterval(timer);
-      }, [startDate, endDate]);
+  const calculateTimeLeft = () => {
+    // const start = new Date(startDate).getTime();
+    // const end = new Date(endDate).getTime();
+    // const totalSeconds = Math.floor((end - Date.now()) / 1000);
 
+    // if (totalSeconds <= 0) {
+    //   setDays(0);
+    //   setHours(0);
+    //   setMinutes(0);
+    //   setSeconds(0);
+    //   return;
+    const start = new Date(startDate).getTime();
+    const end = new Date(endDate).getTime();
+    const totalSeconds = Math.floor((end - Date.now()) / 1000);
+
+    const daysSeconds = (end - start) / 1000;
+    if (totalSeconds <= 0) {
+      setDays(0);
+      setHours(0);
+      setMinutes(0);
+      setSeconds(0);
+      return;
+    }
+
+    const daysLeft = Math.floor(daysSeconds / 86400);
+    const hoursLeft = Math.floor((totalSeconds % 86400) / 3600);
+    const minutesLeft = Math.floor((totalSeconds % 3600) / 60);
+    const secondsLeft = Math.floor(totalSeconds % 60);
+
+    setDays(daysLeft);
+    setHours(hoursLeft);
+    setMinutes(minutesLeft);
+    setSeconds(secondsLeft);
+  };
+
+  useEffect(() => {
+    const timer = setInterval(() => calculateTimeLeft(), 1000);
+    return () => clearInterval(timer);
+  }, [startDate, endDate]);
 
   return (
     <div>
-
+      <div>
+        <p className="text-[18px]">
+          {days}d:{hours}h:{minutes}m:{seconds}s
+        </p>
+      </div>
     </div>
-  )
-}
+  );
+};
+
+export default LiveCountdown;
--- client/frontend/src/Pages/Activity/Auctions.jsx
+++ client/frontend/src/Pages/Activity/Auctions.jsx
@@ -1,24 +1,54 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
+import Countdown from "../../Components/Countdown";
+import axios from "axios";
 
 export default function Auctions() {
+  const [auctions, setAuctions] = useState([]);
+
+  useEffect(() => {}, []);
+
+  const fetchAuctions = async () => {
+    try {
+      const token = localStorage.getItem("token");
+      const allOffers = await axios.get(
+        "http://localhost:8080/bid",
+        {
+          headers: {
+            Authorization: `Bearer ${token}`, // Include the token in the Authorization header
+          },
+        }
+      );
+      // console.log(allOffers.data.offers);
+
+      if (allOffers.status === 200) {
+        setOffers(allOffers.data.offers);
+      } else {
+        return null;
+      }
+    } catch (error) {
+      console.log("Oh no, something went wrong", error);
+    }
+  };
   return (
     <div>
-        <div className='flex w-[1171px] h-[120px] border-[1px] border-black ml-[188px] justify-between'>
-            <div className='flex'>
-                <div className='py-3'>
-                    <h1 className='text-[27px]'>Product Title</h1>
-                    <p className='text-[27px]'>Username</p>
-                </div>
-                <img></img>
-                <div className=' flex items-center px-5 w-[375px] '>
-                    <h1 className='text-[27px]'>Product Description</h1>
-                </div>
-            </div>
-            <div className='flex items-center'>
-                <h1 className='text-[27px]'>Expired</h1>
-                <h1 className='text-[27px] pl-[65px] px-[10px]'>Sold: $00.00</h1>
-            </div>
+      {}
+      <div className="flex w-[1171px] h-[120px] border-[1px] border-black ml-[188px] justify-between">
+        <div className="flex">
+          <div className="py-3">
+            <h1 className="text-[27px]">Product Title</h1>
+            <p className="text-[27px]">Username</p>
+          </div>
+          <img></img>
+          <div className=" flex items-center px-5 w-[375px] ">
+            <h1 className="text-[27px]">Product Description</h1>
+          </div>
         </div>
+        <div className="flex items-center">
+          <h1 className="text-[27px]">{}</h1>
+          <Countdown startDate={"2023-06-01"} endDate={"2023-06-07"} />
+          <h1 className="text-[27px] pl-[65px] px-[10px]">Sold: $00.00</h1>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
